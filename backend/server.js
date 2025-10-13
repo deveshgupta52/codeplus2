5e35d6dd0b0e4a8e77d1aa80fb19cb8864c068ca
Armaan Siddiqui <armaansiddiqui.pms@gmail.com>
2025-10-13 19:21:35 +0530

    server modification

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const app = express();
 
 const allowedOrigins = [
     'http://localhost:5173',                
-    'https://codeplus-jade.vercel.app'      
+    'https://code-t3sb.vercel.app/' 
 ];
 
 app.use(cors({
