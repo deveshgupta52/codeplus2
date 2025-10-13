0cd98e34c3eebf6582a87a09ba5e26ad3da2b7d2
Armaan Siddiqui <armaansiddiqui.pms@gmail.com>
2025-10-13 19:38:15 +0530

    Server update

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const app = express();
 
 const allowedOrigins = [
     'http://localhost:5173',                
-    'https://code-t3sb.vercel.app/' 
+    'https://code-t3sb.vercel.app' 
 ];
 
 app.use(cors({
