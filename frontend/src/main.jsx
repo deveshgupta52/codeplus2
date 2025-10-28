9b3fc390ae628ed7dae567215f3f0e9ef840bc3b
devesh gupta <https://github.com/deveshgupta52/CODSOFT>
2025-10-29 00:19:40 +0530

    b

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -29,4 +18,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 );
->>>>>>> 6885616eb0c5a1f917a5a5a35f138080c924f29d
