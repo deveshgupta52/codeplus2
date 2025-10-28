f734d7f2d4ef92794fdbca19323052d3adf0ae5d
aksh4567 <akshsingh977@gmail.com>
2025-10-28 23:20:20 +0530

    removed some more markers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import { AuthProvider } from './context/AuthContext.jsx';
-import { ThemeProvider } from './context/ThemeContext.jsx';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
@@ -29,4 +17,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 );
->>>>>>> 6885616eb0c5a1f917a5a5a35f138080c924f29d
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-=======
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
->>>>>>> 6885616eb0c5a1f917a5a5a35f138080c924f29d
 })
