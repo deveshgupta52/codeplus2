1b63fd71b708ee2aaaf2e934f8f3422d3918c6f7
devesh gupta <https://github.com/deveshgupta52/CODSOFT>
2025-10-29 00:14:41 +0530

    b

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
+
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
@@ -12,10 +12,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-=======
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 6885616eb0c5a1f917a5a5a35f138080c924f29d
+
+
 })
