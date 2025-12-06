2495df45bdb507e20d36d0b590d96328e6d8e230
Armaan Siddiqui <armaansiddiqui.pms@gmail.com>
2025-12-07 04:31:33 +0530

    dist

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
+    'http://localhost:3000',
     'https://code-t3sb.vercel.app'
 ];
 app.use(cors({
